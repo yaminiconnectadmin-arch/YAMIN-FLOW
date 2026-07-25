de04983fa7c0bd9f75d9a77f77e8162a49446d1b
yaminiconnectadmin-arch <yaminiconnect.admin@gmail.com>
2026-07-26 01:19:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "@/App.css";
 import "@/index.css";
